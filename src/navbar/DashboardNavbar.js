17d82cdb8afcc793fafee4fe1b1a15119c986256
jmalovera10 <jma.lovera10@uniandes.edu.co>
2018-11-10 14:34:21 -0500

    Navbar icons

--- src/navbar/DashboardNavbar.js
+++ src/navbar/DashboardNavbar.js
@@ -2,26 +2,39 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {Navbar, Nav, NavItem, Glyphicon} from "react-bootstrap";
 
-export default class DashboardNavbar extends Component{
+export default class DashboardNavbar extends Component {
 
-    render(){
+    render() {
         return (
             <Navbar>
                 <Navbar.Header>
                     <Navbar.Brand onClick={this.props.goToHome}>
-                        <a>React-Bootstrap</a>
+                        <div className="brand">
+                            Telegram Analysis
+                        </div>
+                        <div>
+                            <Glyphicon glyph="send"/>
+                        </div>
                     </Navbar.Brand>
                 </Navbar.Header>
                 <Nav pullRight>
                     <NavItem eventKey={1} onClick={this.props.goToEvConn}>
-                        <Glyphicon glyph=""/>
-                        Ev. Connectivity
+                        <Glyphicon glyph="globe" bsClass="glyphicon"/>
+                        <div>
+                            Ev. Connectivity
+                        </div>
                     </NavItem>
                     <NavItem eventKey={2} onClick={this.props.goToPerformance}>
-                        Performance
+                        <Glyphicon glyph="dashboard" bsClass="glyphicon"/>
+                        <div>
+                            Performance
+                        </div>
                     </NavItem>
                     <NavItem eventKey={3} onClick={this.props.goToSecurity}>
-                        Security
+                        <Glyphicon glyph="lock" bsClass="glyphicon"/>
+                        <div>
+                            Security
+                        </div>
                     </NavItem>
                 </Nav>
             </Navbar>
@@ -29,9 +42,9 @@ export default class DashboardNavbar extends Component{
     }
 }
 
-DashboardNavbar.propTypes={
-    goToHome : PropTypes.func.isRequired,
-    goToEvConn : PropTypes.func.isRequired,
-    goToPerformance : PropTypes.func.isRequired,
-    goToSecurity : PropTypes.func.isRequired
+DashboardNavbar.propTypes = {
+    goToHome: PropTypes.func.isRequired,
+    goToEvConn: PropTypes.func.isRequired,
+    goToPerformance: PropTypes.func.isRequired,
+    goToSecurity: PropTypes.func.isRequired
 };
