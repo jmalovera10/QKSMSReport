920363fd2e5e96cbd1e399f6d388d8b54cfdb25d
tf-venegas10 <tf.vengas10@uniandes.edu.co>
2018-11-12 12:59:46 -0500

    eventual conn

---
+++ src/ev-conn/CardReport.js
@@ -0,0 +1,31 @@
+import "./CardReport.css"
+import React, {Component} from 'react';
+import {Image} from "react-bootstrap";
+import {Card,Col, Row} from 'reactstrap';
+
+
+// App component - represents the whole app
+
+export default class CardReport extends Component {
+
+    constructor(props) {
+        super(props);
+
+    }
+
+
+    render() {
+
+        return (
+            <Col sm="6">
+                <Card className="contrib-card-report">
+                    <h1>{this.props.title}</h1>
+                    <Image src={this.props.imageUrl} rounded={true} responsive={true}/>
+                    <p>{this.props.analysis}</p>
+                </Card>
+            </Col>
+        );
+
+    }
+
+}
--- src/ev-conn/EventualConnectivity.js
+++ src/ev-conn/EventualConnectivity.js
@@ -1,17 +1,28 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+import "./EventualConnectivity.css"
+import CardReport from "./CardReport";
+import {Row} from 'reactstrap';
 
 export default class EventualConnectivity extends Component {
 
-    render(){
-        return(
-          <div>
+    render() {
+        return (
+            <Row>
 
-          </div>
+                <CardReport title="Error loading image on airplane mode."
+                            imageUrl="screenshots/errorLoadingImage.PNG"
+                            analysis="Sometimes: On airplane mode, on a conversation, trying to append a picture creates and error (even though no exception is launched) but the picture is not appended and the app reverts to the choose a recipient activity.
+Other times, the picture is appended to the message but when clicking send, nothing is sent and the message no longer appears. When the connection is good, the image is sent with no problem. Inspecting the code we notice that the loader function for multimedia objects avoids exceptions but does not handle them.
+When catching the exceptions this was te result on a picture that was kept as sending (this time it worked)."
+                />
+
+                <CardReport title="Opening message thread."
+                            imageUrl="screenshots/errorOnOpeningThread.PNG"
+                            analysis="On opening message thread with airplane mode on."
+                />
+            </Row>
         );
     }
 }
 
-EventualConnectivity.propTypes = {
-
-};
+EventualConnectivity.propTypes = {};
