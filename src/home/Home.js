e4dfa099c42b6fd8da91c0bd3e9bdc646028cf47
tf-venegas10 <tf.vengas10@uniandes.edu.co>
2018-11-20 12:07:18 -0500

    meh

--- src/home/Home.js
+++ src/home/Home.js
@@ -117,15 +117,19 @@ export default class Home extends Component {
                         <h4>These are the contributor statistics for QKSMS project</h4>
                         <h2>Top Contributor Commits:</h2>
                         <Row>
+                            <Col md="1"></Col>
                             {
                                 topContributors.map((contr) =>
+
                                     <Col sm="4" md="2">
                                         <Image className="image" src={contr.author.avatar_url} rounded={true} responsive={true}/>
                                         <h4>{<a href={contr.author.html_url} target="_blank">{contr.author.login}</a>}</h4>
                                         <h5>{contr.total}</h5>
                                     </Col>
+
                                 )
                             }
+                            <Col md="1"></Col>
                         </Row>
                     </Card>
                 </Col>
