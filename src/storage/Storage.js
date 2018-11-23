2bb7d3becf074c2015ea93fb0ed6213df191dbfa
jmalovera10 <jma.lovera10@uniandes.edu.co>
2018-11-23 10:46:46 -0500

    Storage report done

--- src/storage/Storage.js
+++ src/storage/Storage.js
@@ -6,7 +6,7 @@ import "./Storage.css";
 
 export default class Performance extends Component {
 
-    render(){
+    render() {
         return (
             <div className="storage">
                 <Row>
@@ -16,8 +16,10 @@ export default class Performance extends Component {
                         goodPractice={true}
                         analysis={
                             <p>
-                                As a good practice, the application stores the multimedia content on the external storage.
-                                This minimizes the app space consumption for the internal storage and almost assures that
+                                As a good practice, the application stores the multimedia content on the external
+                                storage.
+                                This minimizes the app space consumption for the internal storage and almost assures
+                                that
                                 there will be enough space for large multimedia content.
                             </p>
                         }
@@ -26,16 +28,27 @@ export default class Performance extends Component {
                     />
                     <CardReport
                         title="Data Storage"
-                        level={3}
+                        level={1}
                         goodPractice={true}
                         analysis={
-                            <p>
-                                The app uses the Realm library to manage the data and multimedia storage. This library
-                                provides a mobile database that runs directly on phones tablets and wearables. The
-                            </p>
+                            <div>
+                                <p>
+                                    The app uses the Realm library to manage the data and multimedia storage. This
+                                    library
+                                    provides a mobile database that runs directly on phones tablets and wearables. This
+                                    library has better performance compared to SQLite and is secure, but if the phone is
+                                    rooted
+                                    all the db content may be expoted with the following command:
+                                </p>
+                                <code>$ adb root exec-out run-as com.moez.QKSMS cat files/default.realm > PATH_TO_EXPORT</code>
+                            </div>
                         }
                         imageUrl={"/screenshots/RealmLibrary.PNG"}
-                        recommendation={""}
+                        recommendation={
+                            "For data storage, depending on the type of data, evaluate the use of SharedPreferences, " +
+                            "SQLite (or other variants) and external storage. The criteria is data size, privacy and " +
+                            "structure."
+                        }
                     />
                 </Row>
             </div>
