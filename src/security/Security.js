50ae603e1cc2e168d59d1637f5a2c35099c17122
jmalovera10 <jma.lovera10@uniandes.edu.co>
2018-11-22 21:54:08 -0500

    Security finished

--- src/security/Security.js
+++ src/security/Security.js
@@ -11,12 +11,49 @@ export default class Security extends Component {
                     title="Permission Management"
                     level={2}
                     imageUrl={"/screenshots/PermissionUse.PNG"}
-                    analysis="The application uses all the permissions declared in the AndroidManifest.xml, so there are no
-                    floating permissions to be exploited by an external application.
-                    Nevertheless, the app does requests and use some permissions that it is not clear why it needs them or what it does with them.
-                    Internet permission is a weird think to ask when the app only sends sms."
+                    analysis={
+                        <div>
+                            <p>
+                                The application uses all the permissions declared in the AndroidManifest.xml, so there
+                                are no floating permissions to be exploited by an external application. Nevertheless,
+                                the app does requests and use some permissions that it is not clear why it needs them or
+                                what it does with them. A complete analysis of the 3 permission blocks is done below:
+                            </p>
+                            <ol>
+                                <li>
+                                    <p>
+                                        <strong>Core Permissions: </strong>every permission in this set is used for SMS
+                                        management and are directly related to the core functionality of the
+                                        application.
+                                    </p>
+                                </li>
+                                <li>
+                                    <p>
+                                        <strong>Connection Permissions: </strong>even though BILLING and INTERNET permissions
+                                        are not part of the main functionality of the application, these are used for the
+                                        user to make donations to the project via internet. This shows that the use of
+                                        these permissions are aligned with side features that are not harmful for the user.
+                                    </p>
+                                </li>
+                                <li>
+                                    <p>
+                                        <strong>Manufacturer Permissions: </strong>the permissions in this set are
+                                        related to manufacturer features, more specifically the use of device notifications.
+                                        The badge, read and write permissions permit the use of the notification bar and
+                                        features of different type of phones by the application.
+                                    </p>
+                                </li>
+                            </ol>
+                            <p>
+                                Aside from the permissions mentioned earlier, we found other permissions related to
+                                user location that are not directly involved with any core or side functionality of
+                                the application and are not asked for the user to accept. This is why we consider that
+                                is a security risk for the user to leak its position via this application.
+                            </p>
+                        </div>
+                    }
                     imageUrl2={"/screenshots/locationPermission.jpeg"}
-                    recommendation="Remove Internet and Location permissions or be straightforward to the users to why these permissions
+                    recommendation="Remove LOCATION permissions or be straightforward to the users to why these permissions
                      are needed."
                 />
 
